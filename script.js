1216a4f2f9681637748fb5623609724a8337b97c
Lê Trung Kiên <letrungkien2k10phutho@gmail.com>
2026-01-28 13:27:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const message = document.getElementById('message');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
+
   const code = document.getElementById('code').value.trim().toUpperCase();
   if (!code) return;
+
+  const params = new URLSearchParams(window.location.search);
+  const guild_id = params.get("guild");
 
   message.textContent = 'Đang kiểm tra mã...';
   message.className = '';
@@ -14,7 +18,10 @@ form.addEventListener('submit', async (e) => {
     const res = await fetch('/.netlify/functions/verify-webhook', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ code })
+      body: JSON.stringify({
+        code,
+        guild_id
+      })
     });
 
     const data = await res.json();
@@ -27,7 +34,8 @@ form.addEventListener('submit', async (e) => {
       message.textContent = data.message || '❌ Mã không hợp lệ hoặc đã hết hạn!';
       message.className = 'error';
     }
-  } catch {
+  } catch (err) {
+    console.error(err);
     message.textContent = '❌ Lỗi kết nối. Thử lại sau!';
     message.className = 'error';
   }
